508d4f5ce996376774fa7f470e02da1d7809bd83
Sam Lehman <samlehman617@gmail.com>
2019-03-20 04:34:10 -0400

    Added npm backup to backup.sh script

--- script/backup.sh
+++ script/backup.sh
@@ -3,5 +3,6 @@
 apt list --installed > ~/.dotfiles/packages/apt.txt
 pip freeze -r ~/.dotfiles/packages/pip.txt
 pip3 freeze -r ~/.dotifles/packages/pip3.txt
+npm list -g > ~/.dotfiles/npm.txt
 
 exit 0
